286d1f1282460a1e3def8fb7fc9bb916a0181844
lazypotatoname <kenaivalencio@gmail.com>
2025-06-11 02:46:45 +0000

    batalhaNaval.c mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,94 +1,125 @@
 #include <stdio.h>
 
 int main() {
-    // Declaração da matriz 10x10 do tabuleiro
     int tabuleiro[10][10];
     int i, j;
 
-    // Inicializa o tabuleiro com 0 (água)
+    // Inicializa o tabuleiro com água (0)
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // Tamanho fixo dos navios: 3
-    int tamanho_navio = 3;
+    // Posiciona navios no tabuleiro (valor 3)
+    // Navio horizontal
+    for (i = 0; i < 3; i++) {
+        tabuleiro[2][2 + i] = 3;
+    }
+    // Navio vertical
+    for (i = 0; i < 3; i++) {
+        tabuleiro[5 + i][5] = 3;
+    }
+    // Diagonal principal
+    for (i = 0; i < 3; i++) {
+        tabuleiro[1 + i][1 + i] = 3;
+    }
+    // Diagonal secundária
+    for (i = 0; i < 3; i++) {
+        tabuleiro[1 + i][7 - i] = 3;
+    }
 
-    // Posiciona navio horizontalmente (linha 1, colunas 2 a 4)
-    int linha_h = 1;
-    int coluna_h = 2;
-    int sobreposicao = 0;
+    // MATRIZES DE HABILIDADE (tamanho 5x5)
+    int cone[5][5];
+    int cruz[5][5];
+    int octaedro[5][5];
 
-    if (coluna_h + tamanho_navio <= 10) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_h][coluna_h + i] != 0) {
-                sobreposicao = 1;
-            }
+    // Inicializa todas com 0
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            cone[i][j] = 0;
+            cruz[i][j] = 0;
+            octaedro[i][j] = 0;
         }
-        if (sobreposicao == 0) {
-            for (i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_h][coluna_h + i] = 3;
+    }
+
+    // Matriz Cone (aponta para baixo, centro no topo)
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            if (j >= 2 - i && j <= 2 + i) {
+                cone[i][j] = 1;
             }
         }
     }
 
-    // Posiciona navio verticalmente (coluna 6, linhas 4 a 6)
-    int linha_v = 4;
-    int coluna_v = 6;
-    sobreposicao = 0;
+    // Matriz Cruz (em forma de +)
+    for (i = 0; i < 5; i++) {
+        cruz[i][2] = 1; // coluna central
+        cruz[2][i] = 1; // linha central
+    }
 
-    if (linha_v + tamanho_navio <= 10) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_v + i][coluna_v] != 0) {
-                sobreposicao = 1;
-            }
-        }
-        if (sobreposicao == 0) {
-            for (i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_v + i][coluna_v] = 3;
+    // Matriz Octaedro (losango)
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            if ((i + j >= 2) && (i + j <= 6) && (i - j <= 2) && (j - i <= 2)) {
+                octaedro[i][j] = 1;
             }
         }
     }
 
-    // Posiciona navio na diagonal principal (de cima para baixo)
-    int linha_d1 = 0;
-    int coluna_d1 = 0;
-    sobreposicao = 0;
+    // Função para aplicar habilidade ao tabuleiro
+    // origem_linha e origem_coluna são o centro onde será aplicada
+    int origem_linha, origem_coluna;
 
-    if (linha_d1 + tamanho_navio <= 10 && coluna_d1 + tamanho_navio <= 10) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_d1 + i][coluna_d1 + i] != 0) {
-                sobreposicao = 1;
-            }
-        }
-        if (sobreposicao == 0) {
-            for (i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_d1 + i][coluna_d1 + i] = 3;
-            }
-        }
-    }
-
-    // Posiciona navio na diagonal secundária (de cima para baixo invertido)
-    int linha_d2 = 0;
-    int coluna_d2 = 9;
-    sobreposicao = 0;
-
-    if (linha_d2 + tamanho_navio <= 10 && coluna_d2 - tamanho_navio + 1 >= 0) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_d2 + i][coluna_d2 - i] != 0) {
-                sobreposicao = 1;
-            }
-        }
-        if (sobreposicao == 0) {
-            for (i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_d2 + i][coluna_d2 - i] = 3;
+    // Aplica Cone (centro no topo da matriz cone)
+    origem_linha = 1;
+    origem_coluna = 4;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            int linha_tab = origem_linha + i;
+            int coluna_tab = origem_coluna - 2 + j;
+            if (linha_tab >= 0 && linha_tab < 10 && coluna_tab >= 0 && coluna_tab < 10) {
+                if (cone[i][j] == 1 && tabuleiro[linha_tab][coluna_tab] == 0) {
+                    tabuleiro[linha_tab][coluna_tab] = 5;
+                }
             }
         }
     }
 
-    // Exibe o tabuleiro
-    printf("Tabuleiro Batalha Naval (0 = água, 3 = navio):\n\n");
+    // Aplica Cruz (centro da matriz é centro da cruz)
+    origem_linha = 6;
+    origem_coluna = 2;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            int linha_tab = origem_linha - 2 + i;
+            int coluna_tab = origem_coluna - 2 + j;
+            if (linha_tab >= 0 && linha_tab < 10 && coluna_tab >= 0 && coluna_tab < 10) {
+                if (cruz[i][j] == 1 && tabuleiro[linha_tab][coluna_tab] == 0) {
+                    tabuleiro[linha_tab][coluna_tab] = 5;
+                }
+            }
+        }
+    }
+
+    // Aplica Octaedro (losango centrado)
+    origem_linha = 7;
+    origem_coluna = 7;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            int linha_tab = origem_linha - 2 + i;
+            int coluna_tab = origem_coluna - 2 + j;
+            if (linha_tab >= 0 && linha_tab < 10 && coluna_tab >= 0 && coluna_tab < 10) {
+                if (octaedro[i][j] == 1 && tabuleiro[linha_tab][coluna_tab] == 0) {
+                    tabuleiro[linha_tab][coluna_tab] = 5;
+                }
+            }
+        }
+    }
+
+    // Exibe o tabuleiro final
+    printf("Tabuleiro com habilidades:\n");
+    printf("0 = Água, 3 = Navio, 5 = Área de Habilidade\n\n");
+
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
